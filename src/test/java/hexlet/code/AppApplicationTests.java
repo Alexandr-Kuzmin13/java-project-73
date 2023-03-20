8a0a267de28ff78b5509059f3257dc64d627bf50
Alexandr Kuzmin <rusglagol@gmail.com>
2023-03-20 11:51:33 +0300

    18

---
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AppApplicationTests {
+
+    @Test
+    void testInit() {
+        assertThat(true).isTrue();
+    }
+}
